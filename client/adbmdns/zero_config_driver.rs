5ce2087ca0f09946dbb66ec0ab55b9cf14d886a8
Fabien Sanglard <sanglardf@google.com>
2025-10-25 13:18:25 -0700

    mDNS on all interfaces and support for ipv6
    Test: Manual
    Bug: 444697021
    Flag: EXEMPT HOST_MODE
    Change-Id: I9221bfc57047795bc172a80f96f104944460cc27

--- client/adbmdns/zero_config_driver.rs
+++ client/adbmdns/zero_config_driver.rs
@@ -4,72 +4,154 @@ use crate::zero_config::ZeroConfigCommand::DnsQuery;
 use crate::zero_config::{ZeroConfig, ZeroConfigCommand};
 use crate::{send_update, AdbMdnsUpdate};
 use anyhow::Result;
-use log::warn;
+use if_addrs::Interface;
+use log::{debug, warn};
+use mio::{net::UdpSocket, Events, Poll};
 use simple_dns::{Name, Packet, Question};
-use socket2::{Domain, Protocol, Socket, Type};
-use std::io::Read;
-use std::net::{Ipv4Addr, Ipv6Addr, SocketAddrV4};
-use std::thread;
+use socket2::{Domain, Socket, Type};
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr, SocketAddrV4, SocketAddrV6};
 use std::time::Duration;
+use std::{net, thread, vec};
+
+struct ZeroConfigIO {
+    interface: Interface,
+    socket: UdpSocket,
+}
 
 pub struct ZeroConfigDriver {
     zero_config: ZeroConfig,
+
+    // The sockets/interfaces used to send and receive mDNS packets
+    io: Vec<ZeroConfigIO>,
 }
 
 const MDNS_PORT: u16 = 5353;
-const MDNS_ADDRESS: Ipv4Addr = Ipv4Addr::new(224, 0, 0, 251);
+const MDNS_ADDRESS_V4: Ipv4Addr = Ipv4Addr::new(224, 0, 0, 251);
+const MDNS_ADDRESS_V6: Ipv6Addr = Ipv6Addr::new(0xff02, 0, 0, 0, 0, 0, 0, 0xfb);
 
 impl ZeroConfigDriver {
     pub fn new(zero_config: ZeroConfig) -> ZeroConfigDriver {
-        ZeroConfigDriver { zero_config }
+        ZeroConfigDriver { zero_config, io: Vec::new() }
     }
 
-    fn send_query(&self, query: &[u8]) -> std::io::Result<()> {
-        let socket = Socket::new(Domain::IPV4, Type::DGRAM, Some(Protocol::UDP))?;
-        socket.set_reuse_address(true)?;
-        #[cfg(unix)]
-        socket.set_reuse_port(true)?;
+    fn send_query(&self, query: &[u8]) -> Result<()> {
+        for zeroconfig_io in &self.io {
+            let addr: SocketAddr = match zeroconfig_io.interface.addr {
+                if_addrs::IfAddr::V4(_) => SocketAddrV4::new(MDNS_ADDRESS_V4, MDNS_PORT).into(),
+                if_addrs::IfAddr::V6(_) => SocketAddrV6::new(
+                    MDNS_ADDRESS_V6,
+                    MDNS_PORT,
+                    0,
+                    zeroconfig_io.interface.index.unwrap_or(0),
+                )
+                .into(),
+            };
 
-        socket.bind(&SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, MDNS_PORT).into())?;
-
-        let dest = SocketAddrV4::new(MDNS_ADDRESS, MDNS_PORT);
-        socket.send_to(query, &dest.into())?;
-
+            zeroconfig_io.socket.send_to(query, addr)?;
+        }
         Ok(())
     }
 
-    pub fn run(&mut self) -> Result<()> {
-        let mut socket = Socket::new(Domain::IPV4, Type::DGRAM, None)?;
+    fn new_socket(addr: SocketAddr) -> Result<Socket> {
+        let domain = match addr {
+            SocketAddr::V4(_) => Domain::IPV4,
+            SocketAddr::V6(_) => Domain::IPV6,
+        };
+
+        let socket = Socket::new(domain, Type::DGRAM, None)?;
+
+        // Play nice with other mDNS daemon that may be running on this machine.
+        // Let's all share the same port.
         socket.set_reuse_address(true)?;
         #[cfg(unix)]
         socket.set_reuse_port(true)?;
 
-        let addr = SocketAddrV4::new(MDNS_ADDRESS, MDNS_PORT);
+        // We are going to run a select() on these so let's make them non-blocking
+        socket.set_nonblocking(true)?;
         socket.bind(&addr.into())?;
+        Ok(socket)
+    }
 
-        log::debug!("new socket bind={}, local={:?}", &addr, socket.local_addr()?);
+    fn create_socket(interface: &Interface) -> Result<UdpSocket> {
+        let ip_address = &interface.ip();
+        match ip_address {
+            IpAddr::V4(ip) => {
+                let addr = SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, MDNS_PORT);
+                let sock = ZeroConfigDriver::new_socket(addr.into())?;
+                sock.join_multicast_v4(&MDNS_ADDRESS_V4, ip)?;
+                sock.set_multicast_if_v4(ip)?;
+                Ok(UdpSocket::from_std(net::UdpSocket::from(sock)))
+            }
+            IpAddr::V6(_) => {
+                let addr = SocketAddrV6::new(Ipv6Addr::UNSPECIFIED, MDNS_PORT, 0, 0);
+                let sock = ZeroConfigDriver::new_socket(addr.into())?;
+                sock.join_multicast_v6(&MDNS_ADDRESS_V6, interface.index.unwrap_or(0))?;
+                sock.set_multicast_if_v6(interface.index.unwrap_or(0))?;
+                Ok(UdpSocket::from_std(net::UdpSocket::from(sock)))
+            }
+        }
+    }
 
-        let multicast_group = Ipv4Addr::new(224, 0, 0, 251);
-        socket.join_multicast_v4(&multicast_group, &Ipv4Addr::new(0, 0, 0, 0))?;
+    fn create_sockets(&mut self) -> Result<()> {
+        let interfaces: Vec<Interface> = if_addrs::get_if_addrs()
+            .unwrap_or_default()
+            .into_iter()
+            .filter(|i| !i.is_loopback() && !i.is_link_local() && i.is_oper_up())
+            .collect();
 
-        // Check if ZeroConf has some commands to run before we start
+        for interface in interfaces {
+            let Ok(socket) = ZeroConfigDriver::create_socket(&interface) else {
+                warn!("Unable to create socket for interface {:?}", interface);
+                continue;
+            };
+            debug!("Created socket {:?} on interface {:?}", socket, interface);
+            self.io.push(ZeroConfigIO { interface, socket });
+        }
+        Ok(())
+    }
+
+    fn process_packet(&mut self, packet: Packet) {
+        let commands =
+            self.zero_config.update(packet.answers, packet.additional_records, packet.name_servers);
+
+        for command in commands {
+            self.process_command(&command);
+        }
+    }
+
+    fn run(&mut self) -> Result<()> {
+        self.create_sockets()?;
+
+        // Check if ZeroConf has some commands to run before we start. This is the time to send
+        // the initial query for tracked services.
         for command in self.zero_config.initial_commands() {
             self.process_command(&command);
         }
 
         let mut buf = [0u8; 65535];
+        let mut poller = Poll::new()?;
+        for (index, interface) in self.io.iter_mut().enumerate() {
+            poller.registry().register(
+                &mut interface.socket,
+                mio::Token(index),
+                mio::Interest::READABLE,
+            )?;
+        }
+
+        let mut events = Events::with_capacity(1024);
+
         loop {
-            let len = socket.read(&mut buf)?;
-            let mdns_reply = Packet::parse(&buf[0..len])?;
+            // TODO timeout should be set according to the attention list in ZeroConf. For now
+            // we never timeout
+            poller.poll(&mut events, None)?;
 
-            let commands = self.zero_config.update(
-                mdns_reply.answers,
-                mdns_reply.additional_records,
-                mdns_reply.name_servers,
-            );
-
-            for command in commands {
-                self.process_command(&command);
+            for event in events.iter() {
+                if !event.is_readable() {
+                    continue;
+                }
+                let len = self.io[event.token().0].socket.recv(&mut buf)?;
+                let packets = Packet::parse(&buf[..len])?;
+                self.process_packet(packets);
             }
         }
     }
@@ -105,7 +187,7 @@ impl ZeroConfigDriver {
 
                 let res = self.send_query(&query);
                 if res.is_err() {
-                    warn!("Error sending query {:?}", question);
+                    warn!("Error sending query {question:?} {res:?}");
                 }
             }
             CreateService { instance_name, service_type, ipv4, ipv6, port } => {
